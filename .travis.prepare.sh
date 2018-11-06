36dd1b39d5d2197d6c88bd570d4eccf3f6230d17
Thiago Veronezi <thiago@veronezi.org>
2018-11-06 18:04:36 -0500

    travis

--- .travis.prepare.sh
+++ .travis.prepare.sh
@@ -5,8 +5,7 @@ cp .travis.settings.xml $HOME/.m2/settings.xml
 sed -i 's/my_password/'"$BINTRAY_PASSWORD"'/g' $HOME/.m2/settings.xml
 
 mvn clean -q
-mvn -q -pl karaoke versions:set -DnewVersion=$KARAOKE_VERSION-b$TRAVIS_BUILD_NUMBER
-mvn -q -pl todo versions:set -DnewVersion=$TODO_VERSION-b$TRAVIS_BUILD_NUMBER
+mvn -q versions:set -DnewVersion=$TODO_VERSION-b$TRAVIS_BUILD_NUMBER
 
 docker logout
 echo "$DOCKER_PASSWORD" | docker login --username $DOCKER_USERNAME --password-stdin
