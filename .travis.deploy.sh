a3b747836610a86565fdf9484cbec2892cf9ed52
Thiago Veronezi <thiago@veronezi.org>
2018-11-07 09:39:45 -0500

    build on pr

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
+REPO_ID=bintray-tveronezi-pretests
+REPO_URL=https://api.bintray.com/maven/tveronezi/pretests
+
 function deploy_java_bin () {
-    mvn deploy:deploy-file -DpomFile=$1/pom.xml -Dfile=$1/target/$2-$3.$4 -Dpackaging=$4 -DrepositoryId=bintray-tveronezi-pretests -Durl=https://api.bintray.com/maven/tveronezi/pretests/$2/;publish=1
+    mvn deploy:deploy-file -DpomFile=$1/pom.xml -Dfile=$1/target/$2-$3.$4 -Dpackaging=$4 -DrepositoryId=$REPO_ID -Durl=$REPO_URL/$2/;publish=1
 }
 
 deploy_java_bin api todo-api $TODO_VERSION-b$TRAVIS_BUILD_NUMBER war
@@ -12,10 +15,13 @@ deploy_java_bin jwt-auth todo-auth $TODO_VERSION-b$TRAVIS_BUILD_NUMBER zip
 deploy_java_bin ui todo-ui $TODO_VERSION-b$TRAVIS_BUILD_NUMBER zip
 
 function deploy_docker_image () {
-    echo "[RUNNING] docker tag $1 veronezi/$1:$2-b$3"
-    docker tag $1 veronezi/$1:$2-b$3
-    echo "[RUNNING] docker push veronezi/$1:$2-b$3"
-    docker push veronezi/$1:$2-b$3
+    if [ "$TRAVIS_PULL_REQUEST" -eq "false" ]; then
+        docker tag $1 veronezi/$1:$2-b$3
+        docker push veronezi/$1:$2-b$3
+    else
+       docker tag $1 veronezi/$1:$2-pr$TRAVIS_PULL_REQUEST
+       docker push veronezi/$1:$2-pr$TRAVIS_PULL_REQUEST
+    fi
 }
 
 deploy_docker_image todo-auth $TODO_VERSION $TRAVIS_BUILD_NUMBER
