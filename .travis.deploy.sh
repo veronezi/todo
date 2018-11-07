9071aebfa03a3b4813a8d0ece8fc30ff2f8bbf01
Thiago Veronezi <thiago@veronezi.org>
2018-11-07 16:26:38 -0500

    deploy only the images we use

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -5,7 +5,9 @@ function deploy_docker_image () {
     if [ "$TRAVIS_BRANCH" = "master" ]; then
         echo "deploying docker image veronezi/$1:$2-b$3"
         docker tag $1 veronezi/$1:$2-b$3
+        docker tag $1 veronezi/$1
         docker push veronezi/$1:$2-b$3
+        docker push veronezi/$1
     else
         echo "deploying docker image veronezi/$1:$2-rc$3"
        docker tag $1 veronezi/$1:$2-rc$3
@@ -13,11 +15,10 @@ function deploy_docker_image () {
     fi
 }
 
-deploy_docker_image todo-auth $TODO_VERSION $TRAVIS_BUILD_NUMBER
-deploy_docker_image todo-auth-keys $TODO_VERSION $TRAVIS_BUILD_NUMBER
-deploy_docker_image todo-api $TODO_VERSION $TRAVIS_BUILD_NUMBER
-deploy_docker_image todo-facade $TODO_VERSION $TRAVIS_BUILD_NUMBER
 deploy_docker_image todo-static $TODO_VERSION $TRAVIS_BUILD_NUMBER
+deploy_docker_image todo-auth $TODO_VERSION $TRAVIS_BUILD_NUMBER
+deploy_docker_image todo-facade $TODO_VERSION $TRAVIS_BUILD_NUMBER
+deploy_docker_image todo-api $TODO_VERSION $TRAVIS_BUILD_NUMBER
 deploy_docker_image todo-ft $TODO_VERSION $TRAVIS_BUILD_NUMBER
 
 echo "binaries uploaded"
