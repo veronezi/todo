ca8afc8fce8a6d13478fc9bebe417f0c3ec8210e
Thiago Veronezi <thiago@veronezi.org>
2018-11-08 16:09:03 -0500

    add veronezi to image name

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -4,14 +4,13 @@ set -e
 function deploy_docker_image () {
     if [ "$TRAVIS_BRANCH" = "master" ]; then
         echo "deploying docker image veronezi/$1:$2-b$3"
-        docker tag $1 veronezi/$1:$2-b$3
-        docker tag $1 veronezi/$1
-        docker push veronezi/$1:$2-b$3
-        docker push veronezi/$1
+        docker tag $1 $1:$2-b$3
+        docker push $1:$2-b$3
+        docker push $1
     else
         echo "deploying docker image veronezi/$1:$2-rc$3"
-       docker tag $1 veronezi/$1:$2-rc$3
-       docker push veronezi/$1:$2-rc$3
+       docker tag $1 $1:$2-rc$3
+       docker push $1:$2-rc$3
     fi
 }
 
