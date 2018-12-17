026a2eb886b776cbf6d82f710a48e86f558a81f0
Thiago Veronezi <thiago@veronezi.org>
2018-12-17 09:30:47 -0500

    docker args

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -4,12 +4,10 @@ set -e
 function deploy_docker_image () {
     if [ "$TRAVIS_BRANCH" = "master" ]; then
         echo "deploying docker image veronezi/$1:$2-b$3"
-        docker tag $1 $1:$2-b$3
         docker push $1:$2-b$3
         docker push $1
     else
         echo "deploying docker image veronezi/$1:$2-rc$3"
-       docker tag $1 $1:$2-rc$3
        docker push $1:$2-rc$3
     fi
 }
